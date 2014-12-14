fa4d5fd532ce995e378de92e320fb50fa9c8b864
Paul Frinchaboy <pfrinchaboy@gmail.com>
2014-12-14 13:34:57 +0100

    Update

--- src/net/Client.java
+++ src/net/Client.java
@@ -1,6 +1,5 @@
 package net;
 
-import io.Transfer;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -15,6 +14,7 @@ import java.io.RandomAccessFile;
 import java.net.Socket;
 import java.nio.file.Files;
 
+import io.Transfer;
 import server.ClientManager;
 import server.Server;
 
